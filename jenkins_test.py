f3a876edbd45f22e9ad2496b9aa895169e26a9bc
Usman Khan <usmankhan@usmans-mbp.mynet>
2020-07-30 11:37:00 +0100

    First Commit

---
+++ jenkins_test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 30 11:33:05 2020
+
+@author: usmankhan
+"""
+
+import sys
+
+print('There are: '+str(len(sys.argv))+' arguments')
+print('List of Arguments: '+(str(sys.argv))+' arguments')
